41dfc0105c3ca46314fa3a970be64a101b015329
QuynhPhamVan <vanquynh18031998@gmail.com>
2024-09-15 10:53:15 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Nguyễn Thu Trang (Châu Anh) à!',
+    introDesc: `Trái đất vốn lạ thường. 
+    Mà sao em cứ đi nhầm đường,
+    lạc vào tim anh lẻ loi.
+    Đằng sau chữ yêu đây là thương.`,
+    btnIntro: 'Điều bất ngờ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 10 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hạnh phúc của anh là có em đó hihi.',
+    btnAccept: 'Điều đặc biệt',
+    messLink: 'C:/Users/vanqu/Videos/Video/tang-crush-main/video.html' 
+}
